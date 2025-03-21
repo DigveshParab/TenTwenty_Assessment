967f534029c0d08b10a8ea4315d9a55cba97a54b
digveshparab <digveshparab123@gmail.com>
2025-03-21 12:11:18 +0530

    added animation for gallery component

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -3,11 +3,17 @@ import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 
 const Gallery = () => {
-    const [visible, setVisible] = useState(false);
+    const [visibleHeader, setVisibleHeader] = useState(false);
+    const [visibleDesc, setVisibleDesc] = useState(false);
 
     useEffect(() => {
         const onScroll = () => {
-          if (window.scrollY >= 70) setVisible(true);
+          if (window.scrollY >= 200){
+            // adding delays to header and desc
+            setTimeout(() => setVisibleHeader(true), 700); 
+            setTimeout(() => setVisibleDesc(true), 800); 
+          }
+            
         };
     
         window.addEventListener("scroll", onScroll);
@@ -20,13 +26,11 @@ const Gallery = () => {
         <div className='w-full h-[320px] mt-20 flex flex-col justify-center items-center md:mt-26'>
             {/* title */}
             <div className='w-full flex justify-center items-center text-center'>
-                <h2 className={`text-[27px] text-black mb-4 md:text-7xl md:mb-10 md:mt-30 transition-all duration-1000 ease-in-out   ${visible ? "opacity-100" : "opacity-0"}`}>Quality Products</h2>
-                {/* if possible remove mr-6 */}
+                <h2 className={`text-[27px] text-black mb-4 md:text-7xl md:mb-10 md:mt-30 transition-all duration-1000 ease-in-out   ${visibleHeader ? "opacity-100" : "opacity-0"}`}>Quality Products</h2>
             </div>
             {/* description */}
             <div className='w-full flex justify-center text-center'>
-                {/* if possible remove mr-6 */}
-                <p className={`w-[77%] md:w-[55%] leading-[20px] md:text-[32px]  md:mt-8 md:leading-[40px] text-gray-500 transition-all duration-1000 ease-in-out   ${visible ? "opacity-100" : "opacity-0"}`}>  
+                <p className={`w-[77%] md:w-[55%] leading-[20px] md:text-[32px]  md:mt-8 md:leading-[40px] text-gray-500 transition-all duration-1000 ease-in-out   ${visibleDesc ? "opacity-100" : "opacity-0"}`}>  
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br className='hidden md:block'/>
                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
@@ -36,7 +40,7 @@ const Gallery = () => {
         </div>
 
 
-        {/* image carosol */}
+        {/* image slider : todo */}
 
         <div className='relative w-full flex justify-center items-center overflow-hidden'>
             <div className='relative w-[100%] min-h-[550px] md:min-h-[960px] mt-0 flex justify-center md:mt-65'>
@@ -74,7 +78,7 @@ const Gallery = () => {
                 <div
                     className="relative min-w-[200px] h-[300px] md:min-w-[500px] md:h-[750px] right-5  transition-all rotate-[10deg] duration-700 ease-in-out transform"
                     style={{
-                    transform: "translateY(30px) scale(1.1)",
+                        transform: "translateY(30px) scale(1.1)",
                     }}
                 >
                     <Image
