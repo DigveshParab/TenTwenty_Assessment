350f669012a3915e08a3f6fded1071d8ac5d54ec
digveshparab <digveshparab123@gmail.com>
2025-03-21 07:17:23 +0530

    Added gallery component for desktop and mobile view

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from "../../components/Navbar";
 import Banner from '../../components/Banner';
+import Gallery from '../../components/Gallery';
 
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
         <div className="flex-col align-middle">
           <Navbar/>
           <Banner/>
+          <Gallery/>
         </div>
       </>
     );
