c33b0922799b701f9afddcf10c415885eed80ef1
digveshparab <digveshparab123@gmail.com>
2025-03-21 07:07:39 +0530

    added font and icons package

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,20 @@
+import { Work_Sans } from "next/font/google";  // already present in tailwind
 import "./globals.css";
+
+
+// setting up the work_sans font
+const workSans = Work_Sans({
+    subsets: ["latin"],
+    weight: ["300", "400", "600", "700"], 
+    variable: "--font-work-sans",
+  });
+
+
 
 const  RootLayout = ({ children })=> {
   return (
-    <html lang="en">
-      <body
-      >
-        {children}
-      </body>
+    <html lang="en" className={workSans.variable}>
+      <body>{children}</body>
     </html>
   );
 }
