011a3a0b27bd467a53cc2e95ccf90d5fb83a106e
digveshparab <digveshparab123@gmail.com>
2025-03-30 22:48:54 +0530

    updated home component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,53 @@
 "use client"
-import React, { useState } from 'react'
+// Framer Motion (which runs in the browser)
+import React from 'react'
 import Navbar from "../../components/Navbar";
-import Banner from '../../components/Banner';
-import Gallery from '../../components/Gallery';
-import FlashScreen from '../../components/FlashScreen';
+import Banner from '../../components/banner/Banner';
+import SplashScreen from '../../components/SplashScreen';
+import {motion} from "framer-motion"
+import ImageSlider from '../../components/ImageSlider';
+import { galleryImages } from '../../data/galleryData';
+import {bannerImages} from "../../data/bannerData"
+import About from '../../components/about/About';
 
 const Home = () => {
-  const [showFlash, setShowFlash] = useState(true);
 
     return (
       <>
-        <div className="flex-col align-middle">
-        {showFlash ? <FlashScreen onFinish={() => setShowFlash(false)} /> : 
-          <>
-            <Navbar/>
-            <Banner/>
-            <Gallery/>  
-          </>
-        
-
-        }
+      <motion.div
+        initial={{display:"block"}}
+        animate={{display:"none"}}
+        transition={{duration:6,ease:"easeInOut"}}
+      >
+        <SplashScreen/>
+      </motion.div >
+      <motion.div
+        initial={{clipPath:"inset(50% 50% 50% 50%)"}}
+        animate={{clipPath:"inset(0% 0% 0% 0%)",display:"none"}}
+        transition={{duration:3,ease:"easeOut",delay:3}}
+        className='absolute inset-0 flex items-center justify-cente z-100'
+      >
+        <div className="flex-col align-middle w-screen h-screen">
+          <Navbar/>
+          <Banner bannerImages={bannerImages}/>
+          <About galleryImages={galleryImages}/>
+        </div>
+      </motion.div>
+      <div className="flex-col align-middle w-screen h-screen">
+          <Navbar/>
+          <Banner bannerImages={bannerImages}/>
+          <About/>
+          <ImageSlider galleryImages={galleryImages}/>
         </div>
       </>
+
     );
   }
-  
+
 export default Home;
+
+
+// todo:
+// 1. disbale scrolling till animation is completed
+// or
+// 1. put the motion div of animation for banner only and keep rest of them outside and load when animation is over on state change
