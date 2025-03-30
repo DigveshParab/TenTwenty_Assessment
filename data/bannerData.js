db0ae7529a15e35452ab9310ed17c6f5cbc49445
digveshparab <digveshparab123@gmail.com>
2025-03-30 23:00:58 +0530

    data change

--- data/bannerData.js
+++ data/bannerData.js
@@ -1,7 +1,6 @@
 export const bannerImages = [
     {src:"/fields_d.png",title:"Welcome To TenTwenty Farms",subtitle:"From Our Farms To Your Hands"},
-    {src:"/fields_e.jpg",title:"Welcome To TenTwenty Farms",subtitle:"Fresh Produce, Delivered Daily"},
-    {src:"/fields_f.jpg",title:"Welcome To TenTwenty Farms",subtitle:"Organic and Locally Sourced"},
-    {src:"/fields_g.jpg",title:"Welcome To TenTwenty Farms",subtitle:"Organic and Locally Sourced"},
-
+    {src:"/fields_e.jpg",title:"Welcome To TenTwenty Farms",subtitle:"From Our Farms To Your Hands"},
+    {src:"/fields_f.jpg",title:"Welcome To TenTwenty Farms",subtitle:"From Our Farms To Your Hands"},
+    {src:"/fields_g.jpg",title:"Welcome To TenTwenty Farms",subtitle:"From Our Farms To Your Hands"},
 ]
